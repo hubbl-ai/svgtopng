8c42c5e20f758f9b2983cfd24130bf649a43e916
Ransome Madziyauswa <rmadziyauswa@gmail.com>
2025-06-04 14:26:49 +0200

    init commit

---
+++ index.js
@@ -0,0 +1,33 @@
+const { convert } = require('convert-svg-to-png');
+const express = require('express');
+
+const app = express();
+app.use(express.urlencoded({ extended: true }));
+
+app.post('/convert/:width/:height', async (req, res) => {
+  console.log(req.body['name'])
+  console.log(req.params)
+  const defaultWidth = 100;
+  const defaultHeight = 100;
+  let width = parseInt(req.params.width, 10);
+  let height = parseInt(req.params.height, 10);
+
+  if (isNaN(width)) {
+    width = defaultWidth
+  }
+   if (isNaN(height)) {
+    height = defaultHeight
+  }
+
+  const options = { width, height };
+
+  try {
+    const png = await convert(req.body['name'], options);
+    res.set('Content-Type', 'image/png');
+    res.send(png);
+  } catch (err) {
+    res.status(500).send('Conversion failed');
+  }
+});
+
+app.listen(3000);
